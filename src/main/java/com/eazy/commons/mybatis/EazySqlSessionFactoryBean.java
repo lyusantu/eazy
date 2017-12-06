8381c6f1cfe251d4657526935170d4349b8c21fc
lyusantu <lyusantu@gmail.com>
2017-12-06 17:11:56 +0800

    重写typeAliasesPackage

---
+++ src/main/java/com/eazy/commons/mybatis/EazySqlSessionFactoryBean.java
@@ -0,0 +1,64 @@
+package com.eazy.commons.mybatis;
+
+import org.apache.commons.lang3.StringUtils;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.core.type.classreading.CachingMetadataReaderFactory;
+import org.springframework.core.type.classreading.MetadataReader;
+import org.springframework.core.type.classreading.MetadataReaderFactory;
+import org.springframework.util.ClassUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * typeAliasesPackage 默认只能扫描某一个路径下，或以逗号等分割的 几个路径下的内容，不支持通配符和正则，采用重写的方式解决
+ */
+public class EazySqlSessionFactoryBean extends SqlSessionFactoryBean {
+
+    private final Logger LOG = LoggerFactory.getLogger(this.getClass());
+
+    static final String DEFAULT_RESOURCE_PATTERN = "**/*.class";
+
+    @Override
+    public void setTypeAliasesPackage(String typeAliasesPackage) {
+        ResourcePatternResolver resolver = (ResourcePatternResolver) new PathMatchingResourcePatternResolver();
+        MetadataReaderFactory metadataReaderFactory = new CachingMetadataReaderFactory(resolver);
+        typeAliasesPackage = ResourcePatternResolver.CLASSPATH_ALL_URL_PREFIX + ClassUtils.convertClassNameToResourcePath(typeAliasesPackage) + "/" + DEFAULT_RESOURCE_PATTERN;
+
+        //将加载多个绝对匹配的所有Resource
+        //将首先通过ClassLoader.getResource("META-INF")加载非模式路径部分
+        //然后进行遍历模式匹配
+        try {
+            List<String> result = new ArrayList<String>();
+            Resource[] resources = resolver.getResources(typeAliasesPackage);
+            if (resources != null && resources.length > 0) {
+                MetadataReader metadataReader = null;
+                for (Resource resource : resources) {
+                    if (resource.isReadable()) {
+                        metadataReader = metadataReaderFactory.getMetadataReader(resource);
+                        try {
+                            result.add(Class.forName(metadataReader.getClassMetadata().getClassName()).getPackage().getName());
+                        } catch (ClassNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+            if (result.size() > 0) {
+                super.setTypeAliasesPackage(StringUtils.join(result.toArray(), ","));
+            } else {
+                LOG.warn("参数typeAliasesPackage:" + typeAliasesPackage + "，未找到任何包");
+            }
+            //logger.info("d");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
