893add6c77be3df393bfd07a68d5e550ac9d05be
lyusantu <lyusantu@gmail.com>
2017-12-07 16:40:29 +0800

    init reg.html

---
+++ src/main/java/com/eazy/commons/dto/SignResult.java
@@ -0,0 +1,39 @@
+package com.eazy.commons.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.xiaoleilu.hutool.json.JSONObject;
+
+import java.io.Serializable;
+
+/**
+ * ajax 请求的返回类型封装JSON结果
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class SignResult implements Serializable {
+
+    private int status;
+
+    private JSONObject data;
+
+    public SignResult(int status, JSONObject data) {
+        this.status = status;
+        this.data = data;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public JSONObject getData() {
+        return data;
+    }
+
+    public void setData(JSONObject data) {
+        this.data = data;
+    }
+
+}
