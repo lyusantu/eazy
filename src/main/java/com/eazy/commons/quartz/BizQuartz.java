8b70a58466bf8c73c7b62c56c4bb6f5a5b044342
lyusantu <lyusantu@gmail.com>
2017-12-08 11:55:36 +0800

    为签到模块提供通用

--- src/main/java/com/eazy/commons/quartz/BizQuartz.java
+++ src/main/java/com/eazy/commons/quartz/BizQuartz.java
@@ -44,4 +44,11 @@ public class BizQuartz {
         LOG.info("@Scheduled-------cacheClear()");
     }
 
+    /**
+     * 每天凌晨0点清理前一天未签到人员的连续签到记录
+     */
+    @Scheduled(cron = "0 0 0 * * ? *")
+    public void clearSignInRecord() {
+        LOG.info("@Scheduled------clearSignInRecord");
+    }
 }
