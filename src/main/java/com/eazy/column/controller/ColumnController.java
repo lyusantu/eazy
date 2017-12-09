5af27a6ffe51fc08e3dc5be218efea8598b05ca3
lyusantu <lyusantu@gmail.com>
2017-12-09 23:32:49 +0800

    自定义列

---
+++ src/main/java/com/eazy/column/controller/ColumnController.java
@@ -0,0 +1,19 @@
+package com.eazy.column.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/column")
+public class ColumnController {
+
+    @RequestMapping(value = "/{column}")
+    public String index(HttpServletRequest request, @PathVariable("column") String column){
+        request.setAttribute("tab_column", column);
+        return "t/index";
+    }
+
+}
