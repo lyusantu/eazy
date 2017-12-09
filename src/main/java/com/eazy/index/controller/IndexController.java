a9561dc10c9f39ed6869b147f5a8b7bfee6248ef
lyusantu <lyusantu@gmail.com>
2017-12-09 23:33:23 +0800

    处理登录后的用户数据

--- src/main/java/com/eazy/index/controller/IndexController.java
+++ src/main/java/com/eazy/index/controller/IndexController.java
@@ -5,14 +5,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
 
     private final Logger LOG = LoggerFactory.getLogger(this.getClass());
 
     @RequestMapping(value = "/")
-    public String index() {
+    public String index(HttpServletRequest request) {
         LOG.info("invoke----------index");
+        request.setAttribute("tab_column", "home");
         return "index";
     }
 
