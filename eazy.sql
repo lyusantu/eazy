c460375ae0b247bcda6b9fc6e676ed7aa7b7c370
lyusantu <lyusantu@gmail.com>
2017-12-12 14:57:55 +0800

    sql

--- eazy.sql
+++ eazy.sql
@@ -10,10 +10,58 @@ Target Server Type    : MYSQL
 Target Server Version : 50717
 File Encoding         : 65001
 
-Date: 2017-12-08 17:04:37
+Date: 2017-12-12 14:57:32
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `column`
+-- ----------------------------
+DROP TABLE IF EXISTS `column`;
+CREATE TABLE `column` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `suffix` varchar(255) NOT NULL,
+  `role` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of column
+-- ----------------------------
+INSERT INTO `column` VALUES ('1', '提问', 'quiz', '1');
+INSERT INTO `column` VALUES ('2', '分享', 'share', '1');
+INSERT INTO `column` VALUES ('3', '讨论', 'discuss', '1');
+INSERT INTO `column` VALUES ('4', '建议', 'suggest', '1');
+INSERT INTO `column` VALUES ('5', '公告', 'notice', '2');
+INSERT INTO `column` VALUES ('6', '动态', 'news', '2');
+
+-- ----------------------------
+-- Table structure for `post`
+-- ----------------------------
+DROP TABLE IF EXISTS `post`;
+CREATE TABLE `post` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `type` int(11) NOT NULL,
+  `content` text NOT NULL,
+  `reward` int(11) NOT NULL COMMENT '奖励',
+  `delete` int(11) NOT NULL,
+  `createtime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+  `author` int(11) NOT NULL,
+  `comments` int(11) DEFAULT NULL,
+  `readers` int(11) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL COMMENT '状态:未结/已结',
+  `top` int(11) NOT NULL,
+  `wonderful` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of post
+-- ----------------------------
+INSERT INTO `post` VALUES ('1', '我是第一个吃螃蟹的人吗？', '1', 'face[嘻嘻] face[嘻嘻] face[嘻嘻] \n\nimg[https://static.oschina.net/uploads/space/2017/1212/140834_Pxwh_3684378.png] ', '20', '0', '2017-12-12 14:44:36', '1', '0', '0', '0', '0', '0');
 
 -- ----------------------------
 -- Table structure for `sign`
@@ -30,7 +78,7 @@ CREATE TABLE `sign` (
 -- ----------------------------
 -- Records of sign
 -- ----------------------------
-INSERT INTO `sign` VALUES ('40', '1', '2017-12-01 16:43:41', '2017-12-07 16:55:51');
+INSERT INTO `sign` VALUES ('40', '1', '2017-12-12 14:28:40', '2017-12-12 14:28:40');
 INSERT INTO `sign` VALUES ('41', '2', '2017-01-08 16:38:21', '2017-12-08 16:43:25');
 
 -- ----------------------------
@@ -57,5 +105,5 @@ CREATE TABLE `user` (
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', '352050321@qq.com', '超人', '0ab44bd43d6b18fcd5cd928d6faab1b8', '100', 'http://oih7sazbd.bkt.clouddn.com/FivibFbr6k8GqJBdZBea56zq0S4X', '0', '', '2017-12-07 16:16:06', '深圳', '0', 'eazy作者', '3');
+INSERT INTO `user` VALUES ('1', '352050321@qq.com', '超人', '9cbf8a4dcb8e30682b927f352d6559a0', '99879', 'http://oih7sazbd.bkt.clouddn.com/FivibFbr6k8GqJBdZBea56zq0S4X', '0', '', '2017-12-12 14:57:09', '深圳', '0', 'eazy作者', '3');
 INSERT INTO `user` VALUES ('2', '52781380@qq.com', '钢铁侠', '9cbf8a4dcb8e30682b927f352d6559a0', '100', '/res/images/avatar/10.jpg', '0', '', '2017-12-08 16:43:17', '深圳', '0', null, '0');
