253e0481922062a310d8bbc684fe70f7ffed394c
lyusantu <lyusantu@gmail.com>
2018-01-23 11:39:58 +0800

    :dog:

--- src/main/java/com/eazy/commons/QiniuAccountMgr.java
+++ src/main/java/com/eazy/commons/QiniuAccountMgr.java
@@ -2,8 +2,8 @@ package com.eazy.commons;
 
 public class QiniuAccountMgr {
 
-    public static final String ACCESS_KEY = "FipYDqSnbP3AzJqtewyFwmY-UNmmw8YFZSDZNHF0";
+    public static final String ACCESS_KEY = "";
 
-    public static final String SECRET_KEY = "pzOOS1_HFKOKDKdw8cW0M7Wd2UTiEZnFcNA2g6ZT";
+    public static final String SECRET_KEY = "";
 
 }
